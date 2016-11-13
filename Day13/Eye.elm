dbf9299956c7b2f52433bc73f00d8d6f06010487
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-11-13 12:52:19 -0800

    Added eye animation

--- Day13/Eye.elm
+++ Day13/Eye.elm
@@ -5,12 +5,12 @@ import Color
 import Day13.Shapes as Shapes
 
 
-eye : Collage.Form
-eye =
+eye : Float -> Collage.Form
+eye opened =
     Collage.group
         [ iris 120
         , pupil 50
-        , eyelids 120 200
+        , eyelids 120 200 (opened * 0.9)
         ]
 
 
@@ -26,17 +26,17 @@ pupil size =
         |> Collage.filled Color.black
 
 
-eyelids : Float -> Float -> Collage.Form
-eyelids height width =
+eyelids : Float -> Float -> Float -> Collage.Form
+eyelids height width opened =
     Collage.group
         [ Collage.polygon
-            (Shapes.curve ( -width, 0 ) ( 0, (height * 1.2) ) ( width, 0 ) 20
+            (Shapes.curve ( -width, 0 ) ( 0, (height * 2 * opened) ) ( width, 0 ) 20
                 ++ Shapes.curve ( width, 0 ) ( 0, (height * 2) ) ( -width, 0 ) 20
             )
             |> Collage.filled Color.brown
         , Collage.polygon
             (Shapes.curve ( -width, 0 ) ( 0, -(height * 2) ) ( width, 0 ) 20
-                ++ Shapes.curve ( width, 0 ) ( 0, -(height * 1.2) ) ( -width, 0 ) 20
+                ++ Shapes.curve ( width, 0 ) ( 0, -(height * 2 * opened) ) ( -width, 0 ) 20
             )
             |> Collage.filled Color.brown
         ]
--- Day13/Main.elm
+++ Day13/Main.elm
@@ -3,11 +3,76 @@ module Main exposing (..)
 import Collage
 import Element
 import Day13.Eye as Eye
+import Html exposing (Html)
+import Html.App
+import Time exposing (Time)
+import Animation
 
 
+type alias Model =
+    { blinkStart : Time
+    , now : Time
+    }
+
+
+initialModel : Model
+initialModel =
+    { blinkStart = 0
+    , now = 0
+    }
+
+
+type Msg
+    = Tick Time
+    | StartBlink Time
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick t ->
+            ( { model | now = t }, Cmd.none )
+
+        StartBlink t ->
+            ( { model | blinkStart = t }, Cmd.none )
+
+
+view : Model -> Html msg
+view model =
+    let
+        closeAnimation =
+            Animation.animation model.blinkStart
+                |> Animation.from 1.0
+                |> Animation.to 0
+                |> Animation.duration 200
+
+        openAnimation =
+            Animation.animation model.blinkStart
+                |> Animation.from 0.0
+                |> Animation.to 1.0
+                |> Animation.duration 200
+                |> Animation.delay 200
+    in
+        Collage.collage 750
+            500
+            [ Eye.eye
+                ((Animation.animate model.now closeAnimation)
+                    + (Animation.animate model.now openAnimation)
+                )
+            ]
+            |> Element.toHtml
+
+
+main : Program Never
 main =
-    Collage.collage 750
-        500
-        [ Eye.eye
-        ]
-        |> Element.toHtml
+    Html.App.program
+        { init = ( initialModel, Cmd.none )
+        , subscriptions =
+            \_ ->
+                Sub.batch
+                    [ Time.every 20 Tick
+                    , Time.every 3000 StartBlink
+                    ]
+        , update = update
+        , view = view
+        }
